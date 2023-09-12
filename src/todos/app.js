b9e444351ee004c8e41edd0bd5f01cb1c60ade32
Jesus Suarez <jesucal68@gmail.com>
2023-09-12 08:02:00 -0600

    Añadí funcionalidad para marcar check un todo y eliminar un todo

--- src/todos/app.js
+++ src/todos/app.js
@@ -3,41 +3,55 @@ import html from "./app.html?raw";
 import { renderTodos } from "./use-cases";
 
 const ElementIDs = {
-    TodoList: '.todo-list',
-    NewTodoInput: '#new-todo-input',
-}
+  TodoList: ".todo-list",
+  NewTodoInput: "#new-todo-input",
+};
 
 /**
  * @param {string} elementId
  */
 export const App = (elementId) => {
+  const displayTodos = () => {
+    const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+    renderTodos(ElementIDs.TodoList, todos);
+  };
+
+  (() => {
+    const app = document.createElement("div");
+    app.innerHTML = html;
+    document.querySelector(elementId).append(app);
+    displayTodos();
+  })();
+
+  // referencias HTML
+  const newDescriptionInfo = document.querySelector(ElementIDs.NewTodoInput);
+	const todoListUl = document.querySelector(ElementIDs.TodoList);
+
+  // listeners
+  newDescriptionInfo.addEventListener("keyup", (e) => {
+    if (e.keyCode !== 13) return;
+    if (e.target.value.trim().length === 0) return;
+
+    todoStore.addTodo(e.target.value);
+    displayTodos();
+    e.target.value = "";
+  });
+
+	todoListUl.addEventListener('click', e => {
+		const element = e.target.closest('[data-id]');
+		todoStore.toggleTodo(element.getAttribute('data-id'));
+		displayTodos();
+	})
+
+	todoListUl.addEventListener('click', e => {
+
+		if (e.target.classList.toString() === 'destroy') {
+			const element = e.target.closest('[data-id]');
+			todoStore.deleteTodo(element.getAttribute('data-id'));
+			displayTodos();
+		}
+
+	})
 
 
-    const displayTodos = () => {
-        const todos = todoStore.getTodos( todoStore.getCurrentFilter());
-        renderTodos(ElementIDs.TodoList, todos);
-    }
-
-
-    (() => {
-        const app = document.createElement('div');
-        app.innerHTML = html;
-        document.querySelector(elementId).append(app);
-        displayTodos();
-    })();
-
-    // referencias HTML 
-    const newDescriptionInfo = document.querySelector(ElementIDs.NewTodoInput);
-
-    // listeners
-    newDescriptionInfo.addEventListener('keyup', e => {
-        if (e.keyCode !== 13) return;
-        if (e.target.value.trim().length === 0) return;
-
-        todoStore.addTodo(e.target.value);
-        displayTodos()
-        e.target.value = '';
-
-    })
-
-}
+};
